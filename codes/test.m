b9fb090acf08154d7639384691a09f4806ffc757
Louis LE LAY <le.lay.louis@gmail.com>
2024-07-01 21:33:14 +0900

    Update test.m

--- codes/test.m
+++ codes/test.m
@@ -1,2 +1,25 @@
-x;
-x = 5
+% Read JSON file
+jsonData = fileread('offset.json');
+
+% Parse JSON data
+data = jsondecode(jsonData);
+
+% Access vectors
+offset = data.offset;
+
+% Display vectors
+disp(offset);
+
+% Example of writing to the JSON file
+disp(offset(4) + ", " + offset(8) +  ", " + offset(12))
+
+% Convert back to JSON string
+jsonDataUpdated = jsonencode(data);
+
+% Save updated JSON data to file
+fid = fopen('offset.json', 'w');
+if fid == -1
+    error('Cannot create JSON file');
+end
+fwrite(fid, jsonDataUpdated, 'char');
+fclose(fid);
