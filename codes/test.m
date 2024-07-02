5f09aaf2b1ef4d524cb5e482867e9c05763d44bd
Louis LE LAY <le.lay.louis@gmail.com>
2024-07-02 19:21:14 +0900

    Update test.m

--- codes/test.m
+++ codes/test.m
@@ -1,3 +1,19 @@
-clear all;
-a = lowPassFilter([1,1,1],0.5, 1,[[5,4,3],[5,2,5]]);
-disp(a);
+fileName = 'stored_data_mat.mat';
+data_to_store = struct();
+data_to_store.name= 'Lois';
+data_to_store.value = [5,3];
+
+if isfile(fileName)
+    % Load existing data
+    existingData = load(fileName);
+
+    % Append the new data
+    data_stored = existingData.data_stored;
+    data_stored(end+1).data_stored = data_to_store;
+else
+    % File does not exist, create a new structure with the data
+    data_stored = struct('data_stored', data_to_store);
+end
+
+% Save the data structure to the file
+save(fileName, 'data_stored');
